9114b9c5a19704ce93fb4663f98a3b4019c7b298
pkarmanski <pkarmanski@edz.com.pl>
2024-11-30 11:32:31 +0100

    - removed example code

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Aplikacja Quiz',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
+    return MaterialApp();
   }
 }
