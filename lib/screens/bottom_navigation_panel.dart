1d8117c563c69f610d71dfc4e0b9f3cb827230d9
pkarmanski <pkarmanski@edz.com.pl>
2024-12-01 06:02:43 +0100

    - added soon

--- lib/screens/bottom_navigation_panel.dart
+++ lib/screens/bottom_navigation_panel.dart
@@ -13,25 +13,54 @@ class BottomNavigationBarComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
+      items: <BottomNavigationBarItem>[
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.person, size: 28), // Większa ikona
           label: 'Account',
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.explore),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.explore, size: 28), // Większa ikona
           label: 'Explore Hobbies',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.people),
+          icon: Stack(
+            clipBehavior: Clip.none, // Pozwala wyświetlić widget poza limitem
+            children: [
+              Icon(Icons.people, size: 28), // Większa ikona
+              Positioned(
+                top: -15, // Większe przesunięcie w górę
+                right: -25, // Większe przesunięcie w prawo
+                child: Transform.rotate(
+                  angle: 0.4, // Kąt obrotu +23 stopnie
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4), // Większy padding
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Text(
+                      "Soon",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12, // Większa czcionka
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
           label: 'My Communities',
         ),
       ],
       currentIndex: selectedIndex,
-      selectedItemColor: selectedIndex == 0 ? Colors.teal : Colors.greenAccent, // Kolor zmienia się zależnie od indexu
+      selectedItemColor: selectedIndex == 0 ? Colors.teal : Colors.greenAccent,
       unselectedItemColor: Colors.blueGrey.shade400,
       backgroundColor: Colors.white,
       onTap: onItemTapped,
     );
   }
+
+
 }
